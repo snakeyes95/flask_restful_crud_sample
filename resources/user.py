1b8ce8306a217b046cc8e411390b5ae188b3265a
shubham <shubhamkadam19@gmail.com>
2021-01-20 11:30:13 +0530

    Adding resources

---
+++ resources/user.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource,reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',type='str',required=False,help='Cannot leave this field blank')
+    parser.add_argument('password',type='str',required=True,help='Please add this field')
+    
+
+    def post(self):
+        data=UserRegister.parser.parse_args()
+        pritn(data)
+        if UserModel.find_by_username(data['username']):
+            return {'message':'A user with that username already exists'},400
+
+        user=UserModel(data['username'],data['password'])
+        user.save_to_db()
+
+        return {"message":"user created successfullt"},201
